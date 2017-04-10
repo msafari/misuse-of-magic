1da9ec81653697732e52e7423a73f21e5bfa944a
Anika <anika.martin@stonybrook.edu>
2017-04-10 19:45:08 -0400

    Made some changes to the attack animation code. The animation should play while tweening.

--- Benchmark_2/js/local/Attack.js
+++ Benchmark_2/js/local/Attack.js
@@ -1,33 +1,7 @@
-Attack.prototype = {
-	atkInit: function() {},
-
-	launch: function(attacker) {
-		if(uses <= 0)
-			return;
-		uses--;
-		var direction = attacker.animations.currentAnim.name;
-		atkSprite.position.y = attacker.position.y + 15;
-		if(direction.search('.*_L') > -1) {
-			//A bit much... if the animation name contains the _L, we are probably facing left. Launch that way
-			atkSprite.position.x = attacker.position.x - 15;
-			atkSprite.animations.play('launch');
-			game.add.tween(atkSprite).to({x: attacker.position.x - 100});
-		}
-		else {
-			atkSprite.animations.play('launch');
-			atkSprite.position.x = attacker.position.x + 15;
-			game.add.tween(atkSprite).to({x: attacker.position.x + 100});
-		}
-	},
-
-	wasSuccessful: function() {
-		return success;
-	},
-};
-function Attack(name, type, spritesheet, uses) {
+function Attack(attacker_name, name, type, spritesheet, uses) {
 	this.name = name;
 	this.type = type;
-	this.uses = uses;
+	this.uses = (attacker_name === "WIZARD") ? Infinity : uses;
 	this.spritesheet = spritesheet; //For animations
 	
 	game.load.spritesheet(name, spritesheet, 16, 16).onFileComplete.addOnce(function(){
@@ -35,24 +9,67 @@ function Attack(name, type, spritesheet, uses) {
 		atkSprite.visible = false;
 		atkSprite.animations.add("launch");
 	});
-		this.icon = "assets/Sprites/attacks/flareIcon.png";  //Right now, just to see something
-		success = true;
+	this.icon = "assets/Sprites/attacks/flareIcon.png";  //Right now, just to see something
+	// we assume it's unsuccesful
+	success = false;
 }
 
-function Attack(name, type, spritesheet) {
-	this.name = name;
-	this.type = type;
-	this.uses = Infinity; //An enemies attack should not run out. In other cases, call above constructor
-	this.spritesheet = spritesheet;
+function attack_init() {
+	console.log("AtkInit run!");
+	var AtkImages = ["assets/Sprites/attacks/Electric Attack Prototype.png", "assets/Sprites/attacks/electromagnetism.png", "assets/Sprites/attacks/firefloom.png", "assets/Sprites/attacks/flare.png",  "assets/Sprites/attacks/Movement Spell.png",
+					"assets/Sprites/attacks/Reverse Direction.png"];
+	for(var i = 0; i < AtkImages.length; i++) {
+	 	console.log(AtkImages[i]);
+	 	var num = i+1;
+	 	console.log("atk"+num);
+	 	game.load.spritesheet("atk"+num, AtkImages[i], 16, 16).onLoadComplete.addOnce(function() {
+	 		Attack.prototype.AtkSprites.push(game.world.create(0, 0, "atk"+num));
+	 	}
+	 	);
+	}
+}
+
+Attack.prototype = {
+	AtkSprite: null,
+	AtkSprites: [],
 	
-	game.load.spritesheet(name, spritesheet, 16, 16).onFileComplete.addOnce(function(){
-		this.atkSprite = game.world.create(0, 0, name, 0);
-		atkSprite.visible = false;
-		atkSprite.animations.add("launch");
-	});
-		this.icon = "assets/Sprites/attacks/flareIcon.png"; 
-		success = true;
-}
+	set_sprite: function(sprite) {
+		AtkSprite = sprite;
+		AtkSprite.animations.add("launch");
+	},
 
+	launch: function(attacker) {
+		if(this.uses <= 0)
+			return;
+		this.uses--;
+		var direction = attacker.animations.currentAnim.name;
+		AtkSprite.position.y = attacker.position.y;
+		game.world.add(AtkSprite);
+		var atkTween;
+		if(direction.search('.*_L') > -1) {
+			//A bit much... if the animation name contains the _L, we are probably facing left. Launch that way
+			AtkSprite.position.x = attacker.position.x - 15;
+			console.log("CANNONS FIRED! (left)");
+			atkTween = game.add.tween(AtkSprite).to({x: attacker.position.x - 100});
+		}
+		else {
+			AtkSprite.position.x = attacker.position.x + 15;
+			console.log("CANNONS FIRED! (right)");
+			atkTween = game.add.tween(AtkSprite).to({x: attacker.position.x + 100});
+		}
+		AtkSprite.visible = true;
+		
+		atkTween.onStart.addOnce(function() {
+			AtkSprite.animations.play('launch', 16, true);
+		})
+		atkTween.start().onComplete.addOnce(function() {
+			console.log("Tween completed");
+			AtkSprite.kill();
+		});
+	},
 
+	was_successful: function() {
+		return success;
+	},
+};
 
