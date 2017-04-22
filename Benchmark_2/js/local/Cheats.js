c2d5b7d7a4fbc902485518664935cd12cafee279
Anika <anika.martin@stonybrook.edu>
2017-04-22 12:06:06 -0400

    Enabling/disabling cheats now works correctly

--- Benchmark_2/js/local/Cheats.js
+++ Benchmark_2/js/local/Cheats.js
@@ -7,15 +7,15 @@ function cheatMenu() {
 	var baseYPosition = 85;
 	var yOffset = 40;
 	pauseGame(true);
-	_.forIn(Cheats.CheatList, function(item) { 
-		console.log(item.enabled);
-		optionText = game.add.text(baseXPosition, baseYPosition + yOffset, item.text + item.enabled);
+	_.forIn(Cheats.CheatList, function(cheatObject) { 
+		console.log(cheatObject.enabled);
+		optionText = game.add.text(baseXPosition, baseYPosition + yOffset, cheatObject.text + cheatObject.enabled);
     	optionText.fixedToCamera = true;
    		optionText.cameraOffset.setTo(baseXPosition, baseYPosition + yOffset);
    		baseYPosition += yOffset; //should keep the text evenly spaced
    		optionText.inputEnabled = true;
-    	optionText.events.onInputUp.add(item.action);
-    	cheats.push(item);
+    	optionText.events.onInputUp.add(cheatObject.action, cheatObject);
+    	cheats.push(cheatObject);
     	cheatText.push(optionText);
 	});
 	Cheats.backAction.addOnce(function() {
@@ -31,10 +31,15 @@ Cheats.CheatList = {
 	invinciblity: {
 		enabled: false,
 		text: "Make Tzhara invincible: ",
-		action: function() {
-			this.enabled = true;
-			console.log("Tzhara is now immune to damage");
-			console.log(this.enabled);
+		action: function() { 
+			if(!this.enabled) {
+				this.enabled = true;
+				console.log("Tzhara is now immune to damage");
+			}
+			else {
+				this.enabled = false;
+				console.log("Invicibility disabled");
+			}
 		} 
 	},
 	infiniteAttacks: {
